69d3c03a85327a063eb37985d7155aa7f161e5df
Samiul  Mushfik <samiul.mushfik@kebt.co.kr>
2020-12-18 16:24:07 +0600

    WIP1

--- test/example/example.test.js
+++ test/example/example.test.js
@@ -3,8 +3,9 @@
 // import {before, after, driver, eyes} from './test-config.js'
 // import {createRequire} from 'module'
 // const require = createRequire(import.meta.url)
-// const { Eyes, VisualGridRunner, RunnerOptions, Target, RectangleSize, Configuration, BatchInfo, BrowserType, DeviceName, ScreenOrientation} = require('@applitools/eyes-selenium');
-// const { Builder, By } = require('selenium-webdriver');
+import {RectangleSize, Target} from "@applitools/eyes-selenium"
+import {driver, exampleAfterEach, exampleBeforeEach, eyes} from "../test-config.js"
+import {By} from 'selenium-webdriver'
 
 // describe('DemoApp - Ultrafast Grid', function () {
 //   before(()=>before());
@@ -36,16 +37,49 @@
 //   after(()=>after());
 // });
 
-import {options, chai} from '../common.js';
-
 // describe('visualgrid', function () {
     // beforeEach(function () {
     //     console.log("Before each test visualgrid")
     // })
 
 // });
-it('should blah example', function () {
-    console.log("example: ", options.foo)
-    chai.assert.isTrue(true)
-})
+describe('example', function () {
+    before(function () {
+        exampleBeforeEach()
+    })
+
+    // it('should blah example', function () {
+    //     console.log("example: ", options.foo)
+    //     chai.assert.isTrue(true)
+    // })
+
+    it('ultraFastTest', async function (done) {
+        // Call Open on eyes to initialize a test session
+        await eyes.open(driver, 'Demo App', 'Ultrafast grid demo', new RectangleSize(800, 600));
+
+        // Navigate the browser to the "ACME" demo app.
+        // ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
+        // but then change the above URL to https://demo.applitools.com/index_v2.html
+        // (for the 2nd run)
+        await driver.get("https://demo.applitools.com");
+
+        // check the login page with fluent api, see more info here
+        // https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
+        await eyes.check("Login Window", Target.window().fully());
+
+        // This will create a test with two test steps.
+        await driver.findElement(By.id("log-in")).click();
+
+        // Check the app page
+        await eyes.check("App Window", Target.window().fully());
+
+        // Call Close on eyes to let the server know it should display the results
+        await eyes.closeAsync();
+        // done();
+    })
+
+    after(function () {
+        exampleAfterEach()
+    })
+});
 
--- test/example1/example1.test.js
+++ test/example1/example1.test.js
@@ -1,6 +1,12 @@
-import {options, chai} from '../common.js';
-
-it('should blah example1', function () {
-    console.log("example1: ", options.foo)
-    chai.assert.isTrue(true)
-})
+// import {options, chai} from '../common.js';
+// import {exampleAfterEach, exampleBeforeEach} from "../test-config.js";
+//
+// describe('example1', function () {
+//     beforeEach(()=>exampleBeforeEach())
+//     afterEach(()=>exampleAfterEach())
+//
+//     it('should blah example1', function () {
+//         console.log("example1: ", options.foo)
+//         chai.assert.isTrue(true)
+//     })
+// });
